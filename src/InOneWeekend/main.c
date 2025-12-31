7f7b27450d3ecacaa8dd6c0ac6d663a3f6847d30
Tomer Eliahu <73798700+Tomer-Eliahu@users.noreply.github.com>
2026-01-01 00:32:09 +0200

    Section 4 done

--- src/InOneWeekend/main.c
+++ src/InOneWeekend/main.c
@@ -2,6 +2,23 @@
 
 #include "color.h"
 #include "vec3.h"
+#include "ray.h"
+
+///@brief sets the color for a given scene ray
+void ray_color(color3 color, const struct Ray *ray)
+{
+    vec3 unit_dir;
+
+    // We know that this use won't actually modify ray->direction
+    unit(unit_dir, (double *)ray->direction);
+
+    double a = 0.5 * (unit_dir[1] + 1.0);
+    // white is (1.0, 1.0, 1.0) and blue is (0.5, 0.7, 1.0);
+    // We want a linear interpolation where the bottom is white and the top is blue.
+    color[0] = (1.0 - a) * 1 + a * 0.5;
+    color[1] = (1.0 - a) * 1 + a * 0.7;
+    color[2] = (1.0 - a) * 1 + a * 1;
+}
 
 int main()
 {
@@ -16,23 +33,73 @@ int main()
 
     // Image
 
-    int image_width = 256;
-    int image_height = 256;
+    double aspect_ratio = 16.0 / 9.0; // width/height
+    int image_width = 400;
+
+    // Calculate the image height, and ensure that it's at least 1.
+
+    int image_height = (int)image_width / aspect_ratio;
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // Camera
+
+    double focal_length = 1.0;
+    double viewport_height = 2.0;
+
+    // image_width/image_height is the *actual* aspect ratio we will have
+    double viewport_width = viewport_height * ((double)image_width / image_height);
+    point3 camera_center = {0};
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    vec3 viewport_u = {viewport_width, 0, 0};
+    vec3 viewport_v = {0, -viewport_height, 0};
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    // This is just viewport_u/image_width and viewport_v/image_height
+    vec3 pixel_delta_u = {(double)viewport_width / image_width, 0, 0};
+    vec3 pixel_delta_v = {0, (double)-viewport_height / image_height, 0};
+
+    // Calculate the location of the upper left pixel.
+    point3 temp1, temp2;
+    point3 viewport_upper_left, pixel00_loc;
+    subtract(temp1, camera_center, (vec3){0, 0, focal_length});
+    subtract(temp1, temp1, scale(temp2, viewport_u, (0.5)));
+    subtract(viewport_upper_left, temp1, scale(temp2, viewport_v, (0.5)));
+
+    // pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v)
+    add(pixel00_loc, viewport_upper_left,
+        scale(temp1, add(temp1, pixel_delta_u, pixel_delta_v), 0.5));
 
     // Render
 
     printf("P3\n");                               // This means the colors will be in ASCII
     printf("%i %i\n", image_width, image_height); // how many pixels to make
     printf("255\n");                              // Max color possible
-
-    for (int i = 0; i < image_height; i++)
+    
+    //The book does this (j then i). So we follow that.
+    for (int j = 0; j < image_height; j++)
     {
-        fprintf(stderr, "\rLines scanned: %i out of %i", i + 1, image_height);
+        fprintf(stderr, "\rLines scanned: %i out of %i", j + 1, image_height);
         fflush(stderr);
 
-        for (int j = 0; j < image_width; j++)
+        for (int i = 0; i < image_width; i++)
         {
-            color3 color = {(double)j / (image_width - 1), (double)i / (image_height - 1), 0.0};
+            point3 pixel_center, temp1, temp2;
+
+            // calculate the pixel location
+            scale(temp1, pixel_delta_u, i);
+            scale(temp2, pixel_delta_v, j);
+            add(pixel_center, pixel00_loc, add(temp1, temp1, temp2));
+
+            vec3 ray_dir;
+            subtract(ray_dir, pixel_center, camera_center);
+
+            struct Ray r;
+            memcpy(r.origin, camera_center, 3 * sizeof(double));
+            memcpy(r.direction, ray_dir, 3 * sizeof(double));
+
+            color3 pixel_color;
+            ray_color(pixel_color, &r);
 
             /*
                 By convention, each of the red/green/blue components are represented internally
@@ -41,7 +108,7 @@ int main()
                 (this happens in the write_color function).
             */
 
-            write_color(color);
+            write_color(pixel_color);
         }
     }
 
---
+++ src/InOneWeekend/ray.h
@@ -0,0 +1,53 @@
+#pragma once
+
+/*
+    The one thing that all ray tracers have is a ray abstraction
+    and a computation of what color is seen along a ray.
+    Let’s think of a ray as a function P(t)=A+tb.
+    Here P is a 3D position along a line in 3D.
+    A is the ray origin and b is the ray direction.
+    The ray parameter t is a real number (double in the code).
+
+*/
+
+#include "vec3.h"
+
+
+/**
+ *  @struct Ray
+ *  @brief A ray consists of an 3D origin point and a direction.
+ *  @param direction this is not necessarily a unit vector!
+ */
+struct Ray
+{
+    point3 origin;
+    vec3 direction;
+};
+
+/// @brief Computes dest: the point the ray will be at t (P(t)= Origin+t* Direction).
+void at(vec3 dest, struct Ray *ray, double t)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        dest[i] = ray->origin[i] + ray->direction[i] * t;
+    }
+}
+
+/*
+
+At its core,
+a ray tracer sends rays through pixels and computes the color seen in the direction of those rays.
+The involved steps are:
+
+    1.Calculate the ray from the “eye” (the camera center) through the pixel,
+    2.Determine which objects the ray intersects, and
+    3.Compute a color for the closest intersection point.
+
+Lets do a simple camera for getting the code up and running.
+The camera will be placed some distance orthogonally behind a digital viewport
+(you can think of this viewport as a window through which we view the image; like standing
+at the window in your house, you view a 3D image through a 2D object).
+
+See section 4.2 for more details.
+
+*/
