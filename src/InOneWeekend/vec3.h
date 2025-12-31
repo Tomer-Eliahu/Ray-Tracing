99d603bef7cf05c0a65cc13d35d988b9c38f35a0
Tomer Eliahu <73798700+Tomer-Eliahu@users.noreply.github.com>
2025-12-31 22:16:08 +0200

    Making vector operations easier to chain

--- src/InOneWeekend/vec3.h
+++ src/InOneWeekend/vec3.h
@@ -9,50 +9,64 @@ typedef double vec3[3];
 typedef vec3 point3;
 typedef vec3 color3;
 
-/// @brief Negate a vector in place (multiply each coordinate by -1).
-void negate(vec3 vec)
+// Note that we are careful to always return a vec3 (the array where we want to store the result),
+// so that we can easily chain computations together.
+// We can always just ignore the return value and it won't matter.
+
+/// @brief Negate a vector (multiply each coordinate by -1).
+/// @remark Note that ret can potentially be equal to vec (this would mean we modify vec in place).
+double* negate(vec3 ret, vec3 vec)
 {
     for (int i = 0; i < 3; i++)
     {
-        vec[i] *= -1;
+        ret[i] = -1 * vec[i];
     }
+
+    return ret;
 }
 
-/// @brief Add vec2 to vec1 modifying vec1 in place.
-void add(vec3 vec1, const vec3 vec2)
+/// @brief Add vec2 to vec1.
+double* add(vec3 ret, vec3 vec1, vec3 vec2)
 {
     for (int i = 0; i < 3; i++)
     {
-        vec1[i] += vec2[i];
+        ret[i] = vec1[i] + vec2[i];
     }
+    return ret;
 }
 
-/// @brief Subtract vec2 from vec1 modifying vec1 in place.
-void subtract(vec3 vec1, const vec3 vec2)
+/// @brief Subtract vec2 from vec1.
+double* subtract(vec3 ret, vec3 vec1, vec3 vec2)
 {
     for (int i = 0; i < 3; i++)
     {
-        vec1[i] -= vec2[i];
+        ret[i] = vec1[i] - vec2[i];
     }
+
+    return ret;
 }
 
-/// @brief Scale vec by a scalar t (modifies vec in place).
-void scale(vec3 vec, double t)
+/// @brief Scale vec by a scalar t.
+/// @remark Note that ret can potentially be equal to vec (this would mean we modify vec in place).
+double* scale(vec3 ret, vec3 vec, double t)
 {
     for (int i = 0; i < 3; i++)
     {
-        vec[i] *= t;
+        ret[i] = vec[i] * t;
     }
+
+    return ret;
 }
 
-/// @brief Multiply vec1 by vec2 element wise (vec1[i]*= vec2[i]]).
-/// Modifies vec1 in place.
-void multiply(vec3 vec1, const vec3 vec2)
+/// @brief Multiply vec1 by vec2 element wise.
+double* multiply(vec3 ret, vec3 vec1, vec3 vec2)
 {
     for (int i = 0; i < 3; i++)
     {
-        vec1[i] *= vec2[i];
+        ret[i] = vec1[i] * vec2[i];
     }
+
+    return ret;
 }
 
 /// @brief Returns the length of a vec3.
@@ -73,14 +87,16 @@ static inline void print(const vec3 vec)
     printf("[%f, %f, %f]", vec[0], vec[1], vec[2]);
 }
 
-/// @brief Make vec into a unit vector (in-place).
-void unit(vec3 vec)
+/// @brief Get a unit vector in vec's direction.
+/// @remark Note that ret can potentially be equal to vec (this would mean we modify vec in place).
+double* unit(vec3 ret, vec3 vec)
 {
     double magnitude = len(vec);
     for (int i = 0; i < 3; i++)
     {
-        vec[i] /= magnitude;
+        ret[i] = vec[i] / magnitude;
     }
+    return ret;
 }
 
 /// @brief Returns the dot product of two vec3.
@@ -89,8 +105,11 @@ static inline double dot(const vec3 u, const vec3 v)
     return u[0] * v[0] + u[1] * v[1] + u[2] * v[2];
 }
 
-/// @brief The cross product of two vec3 (constructs a new vector).
-#define CROSS_PRODUCT(vec1, vec2)           \
-    {vec1[1] * vec2[2] - vec1[2] * vec2[1], \
-     vec1[2] * vec2[0] - vec1[0] * vec2[2], \
-     vec1[0] * vec2[1] - vec1[1] * vec2[0]}
+/// @brief The cross product of two vec3.
+double* cross(vec3 ret, vec3 vec1, vec3 vec2)
+{   
+    ret[0] = vec1[1] * vec2[2] - vec1[2] * vec2[1];
+    ret[1] = vec1[2] * vec2[0] - vec1[0] * vec2[2];
+    ret[2] = vec1[0] * vec2[1] - vec1[1] * vec2[0];
+    return ret;
+}
